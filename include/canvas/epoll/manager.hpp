eb1a2e08fbb5a2f01f578d14a893e1d20deeb522
Samkit Jain <samjam86@yahoo.com>
2011-08-31 18:37:57 +0530

    WIP

--- include/canvas/epoll/manager.hpp
+++ include/canvas/epoll/manager.hpp
@@ -1,4 +1,20 @@
 #ifndef CANVAS_EPOLL_MANAGER_HPP_INCLUDED
 #define CANVAS_EPOLL_MANAGER_HPP_INCLUDED
 
+namespace canvas
+{
+    namespace epoll
+    {
+        class manager
+        {
+        public:
+            manager();
+            ~manager();
+
+        private:
+            int m_fd;
+        };
+    } // ~epoll
+} // ~canvas
+
 #endif // ~CANVAS_EPOLL_MANAGER_HPP_INCLUDED
